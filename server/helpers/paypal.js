0d14f29d061001af18356308fdc1d6472123748e
Ranbir Singh <ranbirsingh.code@gmail.com>
2025-01-17 14:40:22 +0530

    update

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -2,8 +2,8 @@ const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
   mode: "sandbox",
-  client_id: "",
-  client_secret: "",
+  client_id: "AYR0M-vn10SfU9RjhMGJtFE4TrjNKAlsL1xv06lG3XLc7-k3vPJ7ckRmTLXq9quAxSHiMrkBXD3oSkOO",
+  client_secret: "EFDyZyUPFKKLFQJPrr1Re4ViYnVoJLx1YND_zfTqHwz6TcsiOUZSMYMMO1r1Z1kBes8cmRrfCyzJixu4",
 });
 
 module.exports = paypal;
