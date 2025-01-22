2534e81f86929e8d8b2fc4481879a5ac0fc83848
Ranbir Singh <ranbirsingh.code@gmail.com>
2025-01-22 12:10:21 +0530

    update

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -6,13 +6,13 @@ import ShoppingOrders from "@/components/shopping-view/orders";
 function ShoppingAccount() {
   return (
     <div className="flex flex-col">
-      <div className="relative h-[300px] w-full overflow-hidden">
+      <div className="relative mx-auto mt-2 justify-center items-center w-[95vw] rounded-lg h-[20vh] lg:h-[40vh] md:h-[30vh] overflow-hidden">
         <img
           src={accImg}
           className="h-full w-full object-cover object-center"
         />
       </div>
-        <div className="flex flex-col rounded-lg border bg-background p-2 lg:p-6 md:p-6 shadow-sm">
+        <div className="flex flex-col bg-background p-2 lg:p-6 md:p-6 shadow-sm">
           <Tabs defaultValue="orders">
             <TabsList>
               <TabsTrigger value="orders">Orders</TabsTrigger>
